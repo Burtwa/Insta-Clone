6a5bb0531a497c8f5f7f53b75273115f2da0032d
Burtwa <burtonmosmans@gmail.com>
2025-05-27 10:34:38 +0200

    commit 3 	modified:   src/components/AuthForm/AuthForm.jsx 	modified:   src/pages/AuthPage/AuthPage.jsx 	new file:   tutorial.txt

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -6,16 +6,22 @@ const AuthPage = () => {
   return (
     <Flex minH={"100vh"} alignItems={"center"} justifyContent={"center"} px={4}>
         <Container maxW={"container.md"}> 
+            <Flex justify={"center"} align={"center"} gap={10}>
             {/* Left hand side */}
         <Box display={{base: "none", md: "block"}}>
             <Image src="/auth.png" h={650} alt='Phone img' />
          </Box>
-
          {/* Right handside */}
          <VStack spacing={4} align={"stretch"}>
             <AuthForm />
+            <Box textAlign={"center"}>Get the app.</Box>
+            <Flex gap={5} justifyContent={"center"}>
+                <Image src="/microsoft.png" alt='Microsoft Store' h={10} />
+                <Image src="/playstore.png" alt='Play Store' h={10} ml={4} />
+            </Flex>
 
          </VStack>
+         </Flex>
         </Container>
     </Flex>
   )
